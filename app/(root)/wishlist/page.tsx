86271d9413a5bec1662b83d9eadc92e873e520bf
vk-bhanu <146329087+vk-bhanu@users.noreply.github.com>
2024-11-05 14:48:11 +0530

    another changes

--- app/(root)/wishlist/page.tsx
+++ app/(root)/wishlist/page.tsx
@@ -4,7 +4,7 @@ import Loader from "@/components/Loader"
 import ProductCard from "@/components/ProductCard"
 import { getProductDetails } from "@/lib/actions/actions"
 import { useUser } from "@clerk/nextjs"
-import { use, useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 
 const Wishlist = () => {
   const { user } = useUser()
--- lib/models/User.ts
+++ lib/models/User.ts
@@ -1,20 +1,18 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    clerkId: String,
+const userSchema = new mongoose.Schema(
+  {
+    clerkId: {
+      type: String,
+      required: true, 
+    },
     wishlist: {
-        type: Array,
-        default: []
+      type: [String], 
+      default: [],
     },
-    createdAt:{
-        type: Date,
-        default: Date.now
-    },
-    updatedAt: {
-        type: Date,
-        default: Date.now
-    }
-});
+  },
+  { timestamps: true } 
+);
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
 export default User;
